c04c4acd2bdf2e4fa5efe5f7ef88481b0219a6d9
sshagiev <sshagiev@phoenixit.ru>
2023-03-10 00:15:26 +0300

    add test for arctg

--- src/main/java/shagiev/dobryagin/Main.java
+++ src/main/java/shagiev/dobryagin/Main.java
@@ -4,8 +4,8 @@ import shagiev.dobryagin.functions.ArctgFunction;
 
 public class Main {
     public static void main(String[] args) {
-        ArctgFunction arctgFunction = new ArctgFunction();
-        double res = arctgFunction.calculate(2, 100);
+        var arctgFunction = new ArctgFunction();
+        double res = arctgFunction.calculate(1, 10);
         System.out.println(res);
     }
 }
--- src/test/java/shagiev/dobryagin/functions/ArctgFunctionTest.java
+++ src/test/java/shagiev/dobryagin/functions/ArctgFunctionTest.java
@@ -1,13 +1,49 @@
 package shagiev.dobryagin.functions;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArctgFunctionTest {
 
-    @Test
-    void calculate() {
+    ArctgFunction arctgFunction = new ArctgFunction();
 
+    @ParameterizedTest
+    @CsvSource({
+            "-0.8, -0.67474",
+            "-0.5, -0.46365",
+            "-0.1, -0.09966",
+            "0, 0",
+            "0.5, 0.46365",
+            "0.75, 0.64350"
+    })
+    void correct_calculate_inside_points_correct(double x, double answer) {
+        assertTrue(Math.abs(arctgFunction.calculate(x, 100) - answer) < 0.01);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1, -0.78540",
+            "1, 0.78540"
+    })
+    void calculate_border_points_correct(double x, double answer) {
+        assertTrue(Math.abs(arctgFunction.calculate(x, 100) - answer) < 0.01);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-0.99, -0.78037",
+            "0.99, 0.78037"
+    })
+    void calculate_near_border_points_correct(double x, double answer) {
+        assertTrue(Math.abs(arctgFunction.calculate(x, 100) - answer) < 0.01);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.1, -5, -100, 1.01, 41, 999})
+    void calculate_outer_points_exception(double x) {
+        assertThrows(IllegalArgumentException.class, () -> arctgFunction.calculate(x, 100));
     }
 }
